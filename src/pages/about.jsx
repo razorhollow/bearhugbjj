5a2396b298d7606cab274df8dc48b430f38fc908
RazorHollow <rob@razorhollow.com>
2023-10-16 10:41:29 -0400

    add layout component to skeleton

---
+++ src/pages/about.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About Bearhug Brazilian Jiujitsu</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quas mollitia? Minima, nihil animi laborum aperiam at laudantium, molestiae aspernatur ratione maxime recusandae et sunt voluptatum maiores rem! Quod, consequuntur?
+      Saepe, nesciunt pariatur accusamus dolorem aliquid id nemo sunt fuga voluptatem et nisi quos voluptates eum recusandae itaque error similique ea illum! Vitae voluptatem nemo fuga corrupti eum quia ratione?</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Us</title>
+export default AboutPage
